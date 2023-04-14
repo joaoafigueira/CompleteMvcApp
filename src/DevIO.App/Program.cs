6aec983a8f9fdaa750698377cee52b7a4c5fb466
João Victor <79715612+joaoafigueira@users.noreply.github.com>
2023-04-14 11:17:51 -0300

    feat: criação do banco de dados e mapeamento das entitidades

--- src/DevIO.App/Program.cs
+++ src/DevIO.App/Program.cs
@@ -1,4 +1,5 @@
 using DevIO.App.Data;
+using DevIO.Data.Context;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,12 +7,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+
+builder.Services.AddDbContext<MeuDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
+
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
